10665796736ab5dfb7501e4d489e08e48fc7ff4e
Bharath B C <bharathbc2017@gmail.com>
2023-07-31 11:26:03 +0530

    com.uis.collection in ArrayCheck.java modified

--- src/com/uis/Collection/ArrayCheck.java
+++ src/com/uis/Collection/ArrayCheck.java
@@ -3,6 +3,7 @@ package com.uis.Collection;
 public class ArrayCheck {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to Git");
 		Object[] o = new Object[5];
 		o[0] = new Student();
 		o[0] = new Cutomer();
